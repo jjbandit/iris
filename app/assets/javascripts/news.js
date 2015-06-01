58dae3fa220f25063763f264b0213360ebe358a7
jjbandit <revelstokesnowboard@gmail.com>
2015-06-01 12:37:34 -0700

    restrict query string persisting accross controllers

--- app/assets/javascripts/news.js
+++ app/assets/javascripts/news.js
@@ -1,4 +1,12 @@
 $( document ).ready( function () {
+	// Ajax request more news stories, don't scroll to top of page on reload
+	$( 'a.page-link' ).on( 'click' , function (evt) {
+		var cPath = window.location.pathname;
+			evt.preventDefault();
+			Turbolinks.visit(evt.target.href, { scroll: false});
+	});
+
+	// Ajax request a news article
 	$( 'a.read-more' ).on( 'click' , function (evt) {
 		var cPath = window.location.pathname;
 		// If we're on root top-out the page when we nav to a news article
