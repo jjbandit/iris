99285baeab7eb9f1e71f9adbeeb4486ead1d28bf
jjbandit <revelstokesnowboard@gmail.com>
2015-05-31 18:48:49 -0700

    add conditional to top-out scroll on nav from homepage

--- app/assets/javascripts/news.js
+++ app/assets/javascripts/news.js
@@ -1,7 +1,14 @@
 $( document ).ready( function () {
 	$( 'a.read-more' ).on( 'click' , function (evt) {
-		evt.preventDefault();
-		Turbolinks.visit(evt.target.href, { scroll: false});
+		var cPath = window.location.pathname;
+		// If we're on root top-out the page when we nav to a news article
+		if (cPath === '/') {
+			Turbolinks.visit(evt.target.href);
+		// Otherwise don't scroll to top
+		} else {
+			evt.preventDefault();
+			Turbolinks.visit(evt.target.href, { scroll: false});
+		}
 	});
 });
 
