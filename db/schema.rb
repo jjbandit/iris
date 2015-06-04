401bca7ad8275b222be7537b6c88129a241c0d97
jjbandit <revelstokesnowboard@gmail.com>
2015-06-03 18:37:48 -0700

    rails g spree:custom_user and modify auth_helpers for spree

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150603234015) do
+ActiveRecord::Schema.define(version: 20150603234719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -981,19 +981,22 @@ ActiveRecord::Schema.define(version: 20150603234015) do
   add_index "spree_zones", ["kind"], name: "index_spree_zones_on_kind", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                             default: "",    null: false
+    t.string   "encrypted_password",                default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",                     default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin",                  default: false
+    t.boolean  "admin",                             default: false
+    t.string   "spree_api_key",          limit: 48
+    t.integer  "ship_address_id"
+    t.integer  "bill_address_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
