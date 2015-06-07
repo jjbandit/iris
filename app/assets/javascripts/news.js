b578f60443e0fe292de37ca3cf2b9cfce4de6ac6
jjbandit <revelstokesnowboard@gmail.com>
2015-06-07 16:23:20 -0700

    add news-article id to news-show, remove turbolinks scroll conditional

--- app/assets/javascripts/news.js
+++ app/assets/javascripts/news.js
@@ -1,22 +1,9 @@
 $( document ).ready( function () {
 	// Ajax request more news stories, don't scroll to top of page on reload
 	$( 'a.page-link' ).on( 'click' , function (evt) {
-		var cPath = window.location.pathname;
-			evt.preventDefault();
-			Turbolinks.visit(evt.target.href, { scroll: false});
+		evt.preventDefault();
+		Turbolinks.visit(evt.target.href, { scroll: false});
 	});
 
-	// Ajax request a news article
-	$( 'a.read-more' ).on( 'click' , function (evt) {
-		var cPath = window.location.pathname;
-		// If we're on root top-out the page when we nav to a news article
-		if (cPath === '/') {
-			Turbolinks.visit(evt.target.href);
-		// Otherwise don't scroll to top
-		} else {
-			evt.preventDefault();
-			Turbolinks.visit(evt.target.href, { scroll: false});
-		}
-	});
 });
 
