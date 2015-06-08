2e7f0cd3b9dfc6e0042768d57bd5bb35159ec151
jjbandit <revelstokesnowboard@gmail.com>
2015-06-08 10:32:17 -0700

    better news seed data

--- Gemfile
+++ Gemfile
@@ -60,6 +60,10 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'capistrano-rails', group: :development
 
 group :development, :test do
+
+	# Random data generator
+	gem 'lorem_ipsum_amet'
+
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,10 @@ Spree::Core::Engine.load_seed if defined?(Spree::Core)
 Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
 
 20.times do |i|
+	title_length = 2 + rand(9)
 	News.create(
-		title: 'title ' + i.to_s,
-		body: 'Quisque sodales mollis hendrerit. Vestibulum porttitor a massa at scelerisque. Integer augue nulla, ornare non nibh non, feugiat facilisis turpis. Nulla eleifend auctor placerat. Nam maximus eleifend sollicitudin. Aenean vulputate, nisl quis convallis feugiat, elit tortor euismod massa, sit amet volutpat dui ante et massa. Fusce fringilla urna sit amet ipsum tempor, at egestas justo commodo. Aliquam vestibulum orci non ligula tempor tristique. Sed quis felis ut quam maximus imperdiet. Donec condimentum convallis ante. Integer rhoncus efficitur varius. Nunc blandit nisl ut ligula sagittis, eu feugiat quam aliquam',
+		title: LoremIpsum.w(title_length, format: :title_case),
+		body: LoremIpsum.random(paragraphs: 4, html: true),
 		image: File.open('app/assets/images/CustomsandBorderPatrolPredator.jpg')
 	);
 end
