55e7e21f9341a289aeb49c63d19f4206c67938e3
jjbandit <revelstokesnowboard@gmail.com>
2015-06-08 17:23:04 -0700

    news transitions working

--- app/assets/javascripts/news.js
+++ app/assets/javascripts/news.js
@@ -1,9 +1,23 @@
 $( document ).ready( function () {
 	// Ajax request more news stories, don't scroll to top of page on reload
+
+	var indexHeight;
+
 	$( 'a.page-link' ).on( 'click' , function (evt) {
 		evt.preventDefault();
+		$( '.news-teaser' ).fadeOut();
 		Turbolinks.visit(evt.target.href, { scroll: false});
 	});
 
+	$(document).on('page:fetch', function () {
+		indexHeight = $('#news-index-wrapper').height();
+	});
+
+	$(document).on('page:change', function () {
+		indexHeight = $('#news-index-wrapper').height();
+		$('#news-index-wrapper').css('height', indexHeight);
+		$( '#news-index-content' ).hide();
+		$( '#news-index-content' ).show( 'explode'  );
+	})
 });
 
