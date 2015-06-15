e24512a1d90c7f45e7a32bc1525a205fa851a099
jjbandit <revelstokesnowboard@gmail.com>
2015-06-14 22:27:05 -0700

    add spree admin role to default user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,23 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
 User.find_or_create_by(email: 'test@gmail.com') do |user|
 	user.admin = true,
 	user.password = 'password',
 	user.password_confirmation = 'password'
+	user.spree_roles << Spree::Role.find_or_create_by(name: "admin")
 end
 
-# Spree::Core::Engine.load_seed if defined?(Spree::Core)
-# Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
+# Spree::Product.create(name: 'Control Loading Yoke', description: 'This is the description', price: '2000.00', shipping_category_id: 1)
 
-# 20.times do |i|
-# 	title_length = 2 + rand(9)
-# 	News.create(
-# 		title: LoremIpsum.w(title_length, format: :title_case),
-# 		body: LoremIpsum.random(paragraphs: 4, html: true),
-# 		image: File.open('app/assets/images/CustomsandBorderPatrolPredator.jpg')
-# 	);
-# end
+Spree::Core::Engine.load_seed if defined?(Spree::Core)
+Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
+
+20.times do |i|
+	title_length = 2 + rand(9)
+	News.create(
+		title: LoremIpsum.w(title_length, format: :title_case),
+		body: LoremIpsum.random(paragraphs: 4, html: true),
+		image: File.open('public/images/CustomsandBorderPatrolPredator.jpg')
+	);
+end
