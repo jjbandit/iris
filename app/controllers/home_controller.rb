4d79bc4140410d62b4b834e2e1de905ec1b6df80
jjbandit <revelstokesnowboard@gmail.com>
2015-06-17 08:33:08 -0700

    make catalog available to all actions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   def index
 		@allNews = News.order('created_at DESC').page(params[:page]).per(3)
-		@products = Spree::Product.all
   end
 
 	def corporate
