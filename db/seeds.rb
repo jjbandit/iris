8cd1b5a09f7dfec2cdf0cbc797d5d2163c5f9cb2
jjbandit <revelstokesnowboard@gmail.com>
2015-06-16 22:31:54 -0700

    add spree_active_shipping gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ User.find_or_create_by(email: 'test@gmail.com') do |user|
 	user.spree_roles << Spree::Role.find_or_create_by(name: "admin")
 end
 
-Spree::Product.create(name: 'Control Loading Yoke', description: 'This is the description', price: '2000.00', shipping_category_id: 1)
+# Spree::Product.create(name: 'Control Loading Yoke', description: 'This is the description', price: '2000.00', shipping_category_id: 1)
 
 Spree::Core::Engine.load_seed if defined?(Spree::Core)
 Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
