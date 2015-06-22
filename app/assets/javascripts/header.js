301af04a9988475f1bb5006d0b1f57d16cac9104
jjbandit <revelstokesnowboard@gmail.com>
2015-06-22 15:19:41 -0700

    refactor header templates

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,18 +1,3 @@
-$( document ).on( 'page:update', function (evt) {
-
-	$('#catalog').hide();
-
-	$('.navbar-nav li.product-link a').on('click', function (evt) {
-		evt.preventDefault();
-		$('#catalog').toggle('fade');
-	});
-
-	$('.owl2').owlCarousel({
-		items: 1,
-		// nav: true,
-		// navContainer: '.catalog-nav',
-		dots: false,
-		margin: 10,
-		loop: true
-	});
+$( document ).on( 'ready', function (evt) {
+	var s = skrollr.init();
 });
