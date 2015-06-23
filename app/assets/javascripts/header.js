649e0f10e346874177ffb25effacc6c961b7d12a
jjbandit <revelstokesnowboard@gmail.com>
2015-06-22 19:46:37 -0700

    sticky header working

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,6 +1,8 @@
 $( document ).on( 'page:update', function (evt) {
 	var s = skrollr.init();
 
+	skrollr.menu.init(s);
+
 	var viewportHeight = $(window).height();
 	var navHeight = $('#nav-menu').height();
 
