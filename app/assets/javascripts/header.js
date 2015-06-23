3bb8fc45331f0881362f5ab6bad9334e07b8549b
jjbandit <revelstokesnowboard@gmail.com>
2015-06-22 18:11:35 -0700

    header dynamically sized to window height, parallax finished

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,3 +1,8 @@
 $( document ).on( 'page:update', function (evt) {
 	var s = skrollr.init();
+
+	var viewportHeight = $(window).height();
+	var navHeight = $('#nav-menu').height();
+
+	$('.head-image').height(viewportHeight - navHeight);
 });
