997c2f6c565f1613ea6270688f1de31f9f7e9ca9
jjbandit <revelstokesnowboard@gmail.com>
2015-06-22 21:10:54 -0700

    add smooth scrolling snippet

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -20,4 +20,19 @@ $( document ).on( 'page:update', function (evt) {
 			$('#nav-menu').css('margin-top', -navHeight);
 		}
 	});
+
+	// from https://css-tricks.com/snippets/jquery/smooth-scrolling/
+	// for smooth scrolling of anchor links within the same page
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 500);
+				return false;
+			}
+		}
+	});
 });
