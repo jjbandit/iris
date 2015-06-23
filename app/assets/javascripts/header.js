ea24652b0d1bebd18358abbabe23b151cafa9718
jjbandit <revelstokesnowboard@gmail.com>
2015-06-22 20:48:51 -0700

    roll custom sticky header because skrollr one sucked

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,10 +1,23 @@
 $( document ).on( 'page:update', function (evt) {
 	var s = skrollr.init();
 
-	skrollr.menu.init(s);
+	// skrollr.menu.init(s);
 
 	var viewportHeight = $(window).height();
 	var navHeight = $('#nav-menu').height();
+	var headHeight = viewportHeight - navHeight;
+	$('.head-image').height(headHeight);
 
-	$('.head-image').height(viewportHeight - navHeight);
+	$(window).on('scroll', function (evt) {
+		var scroll = $(window).scrollTop();
+		if (scroll >= headHeight) {
+			$('#nav-menu').css('position', 'fixed');
+			$('#nav-menu').css('margin', '0');
+		}
+
+		else if (scroll <= headHeight) {
+			$('#nav-menu').css('position', 'static');
+			$('#nav-menu').css('margin-top', -navHeight);
+		}
+	});
 });
