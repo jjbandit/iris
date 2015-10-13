e4f50b31e7002bd904bae8611854521a9db436da
Jesse Hughes <revelstokesnowboard@gmail.com>
2015-10-13 12:30:54 +0000

    Add will_paginate initializer to resolve conflict with kaminari.
    Since the WillPaginate and Kaminari gems are incompatible I added an
    initailizer to patch incompatibilities.

---
+++ config/initializers/will_paginate.rb
@@ -0,0 +1,13 @@
+if defined?(WillPaginate)
+  module WillPaginate
+    module ActiveRecord
+      module RelationMethods
+        alias_method :per, :per_page
+        alias_method :num_pages, :total_pages
+        alias_method :first_page?, :first
+        alias_method :last_page?, :find_last
+        alias_method :prev_page, :previous_page
+      end
+    end 
+  end
+end
