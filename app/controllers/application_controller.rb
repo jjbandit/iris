e92b338a1c1389be3a30f5a64879c00130d4f7aa
Jesse Hughes <revelstokesnowboard@gmail.com>
2015-11-04 21:01:27 +0000

    Add before_action :set_pages.
    Every page load needs to, unfortunately, be accompanied by a db query to
    retrieve the Refinery::Pages associated with the site.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,18 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
-	# For APIs, you may want to use :null_session instead.
-	protect_from_forgery with: :exception
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
 
-	before_action :set_catalog
+  before_action :set_catalog, :set_pages
 
-	private
+  private
 
-	def set_catalog
-		@products = Spree::Product.active.take 3
-	end
+  def set_pages
+    @pages = Refinery::Page.find_by_path("home-content").children
+  end
+
+  def set_catalog
+    @products = Spree::Product.active.take 3
+  end
 
 end
